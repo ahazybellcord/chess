e1e0a9ebfaa69eb07dedc8539341e0797953c934
zdlebold <zdlebold@buffalo.edu>
2015-05-29 16:25:15 -0400

    Added Knight moves

--- Chess/src/model/Knight.java
+++ Chess/src/model/Knight.java
@@ -9,8 +9,23 @@ public class Knight extends Piece{
 	}
 	
 	public void setPossibleMoves() {
-		
-		
+		int[] ek = {-1, 1};
+		int[] dusra = {-2,2};
+		for(int i : ek) {
+			for(int j : dusra) {
+				if(this.getLocation().x+i>-1 && this.getLocation().x+i<8 && this.getLocation().y+j>-1 && this.getLocation().y+j<8) {
+					if(checkMove(this.getLocation().x+i,this.getLocation().y+j)) {
+						this.getPossibleMoves().add(new Point(this.getLocation().x+i,this.getLocation().y+j));
+					}
+				}
+				if(this.getLocation().x+j>-1 && this.getLocation().x+j<8 && this.getLocation().y+i>-1 && this.getLocation().y+i<8) {
+					if(checkMove(this.getLocation().x+j,this.getLocation().y+i)) {
+						this.getPossibleMoves().add(new Point(this.getLocation().x+j,this.getLocation().y+i));
+					}
+				}
+			}
+		}
+
 	}
 	
 	@Override
---
+++ Chess/src/tests/KnightTest.java
@@ -0,0 +1,38 @@
+package tests;
+
+import java.awt.Point;
+
+import model.Bishop;
+import model.Game;
+import model.Knight;
+
+import org.junit.Test;
+
+public class KnightTest {
+
+	@Test
+	public void test() {
+		Game g = new Game();
+		Knight myKnight = (Knight)g.getBoard().getPiece(1, 0);
+		g.getBoard().setPiece(null, 3, 1);
+		myKnight.setPossibleMoves();
+	
+//		System.out.println(myKnight.getPossibleMoves().toString());
+	}
+	
+	@Test
+	public void test1(){
+		Game g = new Game();
+		for(int i = 0; i<8;i++){
+			for(int j = 0; j<8; j++){
+				g.getBoard().setPiece(null, i, j);
+			}
+		}
+		Knight myKnight = new Knight(true, g, new Point(3,3));
+		
+		myKnight.setPossibleMoves();
+	
+		System.out.println(myKnight.getPossibleMoves().toString());
+	}
+
+}
