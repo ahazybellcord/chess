bbc424feede4a1c5add9dda329457da0bba05867
adhishch <adhishch@buffalo.edu>
2015-05-30 18:02:16 -0400

    Forgot to push the most important file for flipping the Board. Here we go.

--- Chess/src/ui/BoardWindow.java
+++ Chess/src/ui/BoardWindow.java
@@ -32,40 +32,84 @@ public class BoardWindow extends JPanel {
 	}
 	
 	public void update(){
-		for(int i=0; i<8; i++) {
-			for(int j=0; j<8; j++) {
-				if(!_game.getBoard().isEmpty(j, i)){
-							
-			       _buttons[j][i].setText(_game.getBoard().getPiece(j, i).getUnicode());
-			       _buttons[j][i].setFont(new Font("Arial", Font.PLAIN, 40));
-			       _buttons[j][i].setForeground(Color.DARK_GRAY);
-			       _buttons[j][i].setBackground(Color.gray);
-			       _buttons[j][i].setOpaque(false);
-			       
-							
-				}
-				else{
-					_buttons[j][i].setText("");
-					_buttons[j][i].setBackground(Color.gray);
-					_buttons[j][i].setOpaque(false);
+		if(_game.getCurrentPlayer()){
+			for(int i=0; i<8; i++) {
+				for(int j=0; j<8; j++) {
+					if(!_game.getBoard().isEmpty(j, i)){
+								
+				       _buttons[j][i].setText(_game.getBoard().getPiece(j, i).getUnicode());
+				       _buttons[j][i].setFont(new Font("Arial", Font.PLAIN, 40));
+				       _buttons[j][i].setForeground(Color.DARK_GRAY);
+				       _buttons[j][i].setBackground(Color.gray);
+				       _buttons[j][i].setOpaque(false);
+				       
+								
+					}
+					else{
+						_buttons[j][i].setText("");
+						_buttons[j][i].setBackground(Color.gray);
+						_buttons[j][i].setOpaque(false);
+						
+					}
 					
 				}
-				
 			}
-		}
-		if(_game.getPreviousClick()!=null){
-			Piece p = _game.getPreviousClick();
-			_buttons[p.getLocation().x][p.getLocation().y].setForeground(Color.BLUE);
-			for(Point k: p.getPossibleMoves()){
-				_buttons[k.x][k.y].setForeground(Color.RED);
-				if(_game.getBoard().isEmpty(k.x, k.y)){
-					_buttons[k.x][k.y].setBackground(Color.DARK_GRAY);
-					_buttons[k.x][k.y].setOpaque(true);
+			if(_game.getPreviousClick()!=null){
+				Piece p = _game.getPreviousClick();
+				_buttons[p.getLocation().x][p.getLocation().y].setForeground(Color.BLUE);
+				for(Point k: p.getPossibleMoves()){
+					_buttons[k.x][k.y].setForeground(Color.RED);
+					if(_game.getBoard().isEmpty(k.x, k.y)){
+						_buttons[k.x][k.y].setBackground(Color.DARK_GRAY);
+						_buttons[k.x][k.y].setOpaque(true);
 
 
+					}
 				}
 			}
 		}
+		else{
+			for(int i=0; i<8; i++) {
+				for(int j=0; j<8; j++) {
+					if(!_game.getBoard().isEmpty(j, i)){
+								
+				       _buttons[j][Math.abs(i-7)].setText(_game.getBoard().getPiece(j, i).getUnicode());
+				       _buttons[j][Math.abs(i-7)].setFont(new Font("Arial", Font.PLAIN, 40));
+				       _buttons[j][Math.abs(i-7)].setForeground(Color.DARK_GRAY);
+				       _buttons[j][Math.abs(i-7)].setBackground(Color.gray);
+				       _buttons[j][Math.abs(i-7)].setOpaque(false);
+				       
+								
+					}
+					else{
+						_buttons[j][Math.abs(i-7)].setText("");
+						_buttons[j][Math.abs(i-7)].setBackground(Color.gray);
+						_buttons[j][Math.abs(i-7)].setOpaque(false);
+						
+					}
+					
+				}
+			}
+			if(_game.getPreviousClick()!=null){
+				Piece p = _game.getPreviousClick();
+				_buttons[p.getLocation().x][Math.abs(p.getLocation().y-7)].setForeground(Color.BLUE);
+				for(Point k: p.getPossibleMoves()){
+					_buttons[k.x][Math.abs(k.y-7)].setForeground(Color.RED);
+					if(_game.getBoard().isEmpty(k.x, k.y)){
+						_buttons[k.x][Math.abs(k.y-7)].setBackground(Color.DARK_GRAY);
+						_buttons[k.x][Math.abs(k.y-7)].setOpaque(true);
+
+
+					}
+				}
+			}
+		}
+			
+		
+		
+			
+		
+		
 		
 		
 	}
