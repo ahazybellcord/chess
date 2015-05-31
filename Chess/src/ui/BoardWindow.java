17defa30dae4f90cf631a2e8740868e8670f327f
adhishch <adhishch@buffalo.edu>
2015-05-30 22:01:26 -0400

    Added chess notation to the window.

--- Chess/src/ui/BoardWindow.java
+++ Chess/src/ui/BoardWindow.java
@@ -1,12 +1,16 @@
 package ui;
 
+import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.GridLayout;
 import java.awt.Point;
 
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
 import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
 import javax.swing.border.LineBorder;
 
 import model.Game;
@@ -15,20 +19,58 @@ import model.Piece;
 public class BoardWindow extends JPanel {
 	private Game _game;
 	private BoardButton[][] _buttons;
+	private JButton[][] _sideButtons;
+	private JButton[][] _bottomButtons;
 	
 	public BoardWindow(Game game) {
 		_game = game;
 		_buttons = new BoardButton[8][8];
-		this.setLayout(new GridLayout(8,8,2,2));
+		JPanel topHalf = new JPanel();
+		JPanel sidePanel = new JPanel();
+		sidePanel.setLayout(new GridLayout(8,1,2,2));
+		this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+		_sideButtons = new JButton[1][8];
+		for(int i = 0; i<8; i++){
+			_sideButtons[0][i] = new JButton("" + (8-i));
+			_sideButtons[0][i].setPreferredSize(new Dimension(40,80));
+			_sideButtons[0][i].setFont(new Font("Arial", Font.PLAIN, 25));
+			_sideButtons[0][i].setBorder(new EmptyBorder(1, 1, 1, 1));
+
+			sidePanel.add(_sideButtons[0][i]);
+		}
+		topHalf.add(sidePanel);
+		
+		
+		JPanel boardPanel = new JPanel();
+		boardPanel.setLayout(new GridLayout(8,8,2,2));
 		for(int i=0; i<8; i++) {
 			for(int j=0; j<8; j++) {
 				_buttons[j][i] = new BoardButton(_game,j,i);
-				_buttons[j][i].setPreferredSize(new Dimension(100,100));
+				_buttons[j][i].setPreferredSize(new Dimension(80,80));
 				_buttons[j][i].setBorder(new LineBorder(Color.GRAY, 1));
-				this.add(_buttons[j][i]);
+				boardPanel.add(_buttons[j][i]);
 				
 			}
 		}
+		topHalf.add(boardPanel);
+		JPanel bottomHalf = new JPanel();
+		JButton emptyButton = new JButton();
+		emptyButton.setPreferredSize(new Dimension(40,80));
+		emptyButton.setBorder(new EmptyBorder(1,1,1,1));
+	    bottomHalf.add(emptyButton);
+		JPanel bottomPanel = new JPanel();
+		bottomPanel.setLayout(new GridLayout(1,8,2,2));
+		_bottomButtons = new JButton[1][8];
+		for(int i = 0; i<8; i++){
+			_bottomButtons[0][i] = new JButton("" + (char)('a'+i));
+			_bottomButtons[0][i].setPreferredSize(new Dimension(80,40));
+			_bottomButtons[0][i].setFont(new Font("Arial", Font.PLAIN, 25));
+			_bottomButtons[0][i].setBorder(new EmptyBorder(1,1,1,1));
+			bottomPanel.add(_bottomButtons[0][i]);
+		}
+		bottomHalf.add(bottomPanel);
+		this.add(topHalf);
+		this.add(bottomHalf);
 	}
 	
 	public void update(){
@@ -67,6 +109,18 @@ public class BoardWindow extends JPanel {
 					}
 				}
 			}
+			for(int i = 0; i<8; i++){
+				_bottomButtons[0][i].setText("" + (char)('a'+i));
+				_bottomButtons[0][i].setPreferredSize(new Dimension(80,40));
+				_bottomButtons[0][i].setFont(new Font("Arial", Font.PLAIN, 25));
+				_bottomButtons[0][i].setBorder(new EmptyBorder(1,1,1,1));
+			}
+			for(int i = 0; i<8; i++){
+				_sideButtons[0][i].setText("" + (8-i));
+				_sideButtons[0][i].setPreferredSize(new Dimension(40,80));
+				_sideButtons[0][i].setFont(new Font("Arial", Font.PLAIN, 25));
+				_sideButtons[0][i].setBorder(new EmptyBorder(1, 1, 1, 1));
+			}
 		}
 		else{
 			for(int i=0; i<8; i++) {
@@ -102,6 +156,18 @@ public class BoardWindow extends JPanel {
 
 					}
 				}
+			}
+			for(int i = 0; i<8; i++){
+				_bottomButtons[0][i].setText("" + (char)('h'-i));
+				_bottomButtons[0][i].setPreferredSize(new Dimension(80,40));
+				_bottomButtons[0][i].setFont(new Font("Arial", Font.PLAIN, 25));
+				_bottomButtons[0][i].setBorder(new EmptyBorder(1,1,1,1));
+			}
+			for(int i = 0; i<8; i++){
+				_sideButtons[0][i].setText("" + (i+1));
+				_sideButtons[0][i].setPreferredSize(new Dimension(40,80));
+				_sideButtons[0][i].setFont(new Font("Arial", Font.PLAIN, 25));
+				_sideButtons[0][i].setBorder(new EmptyBorder(1, 1, 1, 1));
 			}
 		}
 			
