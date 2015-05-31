bfb212f9bab3e8449e80b215ac21f86d16446caf
zdlebold <zdlebold@buffalo.edu>
2015-05-30 21:25:43 -0400

    Getting a checkout conflict with save.txt. Hope this fixes it.

--- Chess/src/ui/Driver.java
+++ Chess/src/ui/Driver.java
@@ -1,5 +1,6 @@
 package ui;
 
+import java.awt.BorderLayout;
 import java.util.Observable;
 import java.util.Observer;
 
@@ -12,6 +13,7 @@ public class Driver implements Runnable, Observer {
 	private JFrame _frame;
 	private Game _game;
 	private BoardWindow _window;
+	private InfoWindow _infoWindow;
 	
 	public Driver() {
 		_game = new Game();
@@ -23,7 +25,9 @@ public class Driver implements Runnable, Observer {
 	public void run() {
 		_frame = new JFrame("Chess " + "(White to move)");
 		_window = new BoardWindow(_game);
-		_frame.add(_window);
+		_infoWindow = new InfoWindow(_game);
+		_frame.add(_window, BorderLayout.WEST);
+		_frame.add(_infoWindow, BorderLayout.EAST);
 		_frame.pack();
 		_frame.setVisible(true);
 		_frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -36,12 +40,14 @@ public class Driver implements Runnable, Observer {
 	@Override
 	public void update(Observable o, Object arg) {
 		_window.update();
+		_infoWindow.update();
 		if(_game.getCurrentPlayer()){
 			_frame.setTitle("Chess (White to move)" );
 		}
 		else{
 			_frame.setTitle("Chess (Black to move)");
 		}
+		_frame.pack();
 		
 	}
 
